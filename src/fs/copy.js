664d81e3d985f05fd7f996e2d573fe81e0c4a031
Andrei_Mironau <mironau.andrei@gmail.com>
2024-01-28 15:55:03 +0100

    [task 1] create, copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+async function copyDirectory(from, to) {
+    const sourceContent = await fs.readdir(from);
+    await fs.mkdir(to);
+    await Promise.all(sourceContent.map(async (f) => {
+        const fromPath = path.join(from, f);
+        const toPath = path.join(to, f);
+        const stats = await fs.stat(fromPath);
+        if (stats.isDirectory()) {
+            await copyDirectory(fromPath, toPath);
+        } else {
+            await fs.copyFile(fromPath, toPath);
+        }
+    }))
+}
+
+async function isPathPresent(path ){
+    try {
+        await fs.access(path);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolder = path.join('files');
+    const targetFolder = path.join('files_copy');
+    const accessTarget = await isPathPresent(targetFolder);
+    const accessSource = await isPathPresent(sourceFolder);
+    if (accessTarget || !accessSource) {
+        throw Error(`FS operation failed`);
+    }
+
+    await copyDirectory(sourceFolder, targetFolder);
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const fileName = 'fresh.txt';
+    const filePath = path.join('files', fileName);
+    if (fs.existsSync(filePath)) {
+        throw Error(`FS operation failed`);
+    } else {
+        fs.writeFileSync(filePath, 'I am fresh and young', {encoding: 'UTF8'});
+    }
 };
 
 await create();
