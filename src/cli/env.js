a9348f4aac0dffc3b6d174185640e6006ddc7288
Andrei_Mironau <mironau.andrei@gmail.com>
2024-01-28 18:32:36 +0100

    [task 1] env, arg

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const RSVars = Object.keys(process.env)
+        .filter(k => k.startsWith('RSS_'))
+        .map(k => k + '=' + process.env[k])
+        .join('; ');
+    console.log(RSVars)
 };
 
 parseEnv();
