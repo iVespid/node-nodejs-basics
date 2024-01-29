de975fd8601c0dd30179a26f3b88901fb019d96a
Andrei_Mironau <mironau.andrei@gmail.com>
2024-01-29 09:00:41 +0100

    [task 1] zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import path from "path";
+import {getDirPath} from "../fs/common.js";
+import { createReadStream, createWriteStream} from 'fs';
+import {createGzip} from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
 const compress = async () => {
-    // Write your code here 
+    const sourcePath = path.join(getDirPath(import.meta.url), 'files', 'fileToCompress.txt');
+    const targetPath = path.join(getDirPath(import.meta.url), 'files', 'archive.gz');
+    const sourceStream = createReadStream(sourcePath);
+    const targetStream = createWriteStream(targetPath);
+    const zipStream = createGzip();
+    await promisify(pipeline)(sourceStream, zipStream, targetStream);
 };
 
 await compress();
